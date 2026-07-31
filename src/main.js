a32579a034ab3ab14cfeebc4fcc7377045685ef4
kaKa <kaka@greatonce.com>
2026-07-31 18:01:36 +0800

    feat: 创建场景并添加红色立方体网格

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
 import * as THREE from 'three';
 
-console.log(THREE);
+// 创建场景
+const scene = new THREE.Scene();
+
+// 创建几何体外观
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+// 创建几何体材质
+const cubeMaterial = new THREE.MeshBasicMaterial({color: 'red' });
+
+// 创建网格
+const cubeMesh =new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+// 将网格添加到场景中
+scene.add(cubeMesh);
+
+console.log(scene);
